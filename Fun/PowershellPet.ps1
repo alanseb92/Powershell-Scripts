4b4d3d94ae07fb6562b6844efac53ae907774944
Alan Sebastián García <104204055+alanseb92@users.noreply.github.com>
2022-08-28 22:04:05 -0300

    Add files via upload

---
+++ Fun/PowershellPet.ps1
@@ -0,0 +1,48 @@
+﻿#Powershell Pet
+
+$Move = 10
+$Colors = [enum]::GetNames([system.consolecolor])
+
+for (;;) {
+
+$Mover = ""
+
+    switch (Get-Random 5) {
+
+        0 {break}
+        1 {$Move = $Move + 1; break}
+        2 {$Move = $Move - 1; break}
+        3 {$Move = $Move + 2; break}
+        4 {$Move = $Move - 2; break}
+    }
+
+    for ($i=0; $i -lt $Move; $i++) {
+        $Mover += " "
+    }
+
+    switch -Regex (Get-Random 5) {
+
+        [0-1] {break}
+        [2-4] {$Color = $Colors[(Get-Random 16)]}
+    }
+
+cls
+
+    switch (Get-Random 6) {
+
+        0 {write-host "`n$Mover(>'')>" -ForegroundColor $Color; break}
+        
+        1 {write-host "`n$Mover<(''<)" -F $Color; break}
+            
+        2 {write-host "`n$Mover(V''V)" -F $Color; break}
+            
+        3 {write-host "`n$Mover(^''^)" -F $Color; break}
+    
+        4 {write-host "`n$Mover<(  Y )" -F $Color; break}
+
+        5 {write-host "`n$Mover( Y  )>" -F $Color; break}
+    }
+
+sleep 1
+
+}
---
+++ Variables/EnvironmentVariables.ps1
@@ -0,0 +1,17 @@
+﻿#Environment variables can be called upon from the reserved variable $env:
+$env:NUMBER_OF_PROCESSORS
+$env:HOMEPATH
+$env:COMPUTERNAME
+$env:OS
+
+
+#Variables in Powershell are evaluated first when inside " "
+Write-Host "$env:HOMEPATH\desktop"
+
+#Displays all environment variables
+Get-ChildItem env:
+
+
+
+
+
